b5872c4f7387b8470bd18633d5f77982586fa6d6
Ackerman, Adam P. <adam@ou.edu>
2025-11-06 12:24:24 -0600

    uploading example code

---
+++ In_Class_Examples/Intro_to_MVC_EF/Controllers/MusicController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Intro_to_MVC_EF.Models;
+
+namespace Intro_to_MVC_EF.Controllers
+{
+    public class MusicController : Controller
+    {
+        private readonly DB_128040_practiceContext _context;
+
+        public MusicController(DB_128040_practiceContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Music
+        public async Task<IActionResult> Index()
+        {
+            var orderedMusic = _context.Spotifies.OrderByDescending(x => x.Popularity).ToListAsync().Result;
+
+            return View(orderedMusic);
+        }
+
+        // GET: Music/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var spotify = await _context.Spotifies
+                .FirstOrDefaultAsync(m => m.Index == id);
+            if (spotify == null)
+            {
+                return NotFound();
+            }
+
+            return View(spotify);
+        }
+
+      
+        private bool SpotifyExists(int id)
+        {
+            return _context.Spotifies.Any(e => e.Index == id);
+        }
+    }
+}
---
+++ In_Class_Examples/Intro_to_MVC_EF/Program.cs
@@ -0,0 +1,27 @@
+using Intro_to_MVC_EF.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<DB_128040_practiceContext>();
+//builder.Services.AddDbContext<CONTEXTOBJECT>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
