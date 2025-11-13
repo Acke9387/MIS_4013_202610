b494e3f113a7d5211f5112b1f3287573be494b10
Ackerman, Adam P. <adam@ou.edu>
2025-11-13 12:24:02 -0600

    Submitted in class exam review connecting to a database

---
+++ In_Class_Examples/Module2_Exam_Review/Controllers/VineyardController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Module2_Exam_Review.Models;
+
+namespace Module2_Exam_Review.Controllers
+{
+    public class VineyardController : Controller
+    {
+        private readonly DB_128040_pegasausContext _context;
+
+        public VineyardController(DB_128040_pegasausContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Vineyard
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Wines.Take(100).ToListAsync());
+        }
+
+        // GET: Vineyard/Details/5
+        public async Task<IActionResult> Details(double? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var wine = await _context.Wines
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (wine == null)
+            {
+                return NotFound();
+            }
+
+            return View(wine);
+        }
+
+        // GET: Vineyard/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Vineyard/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Country,Description,Designation,Points,Price,Province,Region1,Region2,TasterName,TasterTwitterHandle,Title,Variety,Winery")] Wine wine)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(wine);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(wine);
+        }
+
+        // GET: Vineyard/Edit/5
+        public async Task<IActionResult> Edit(double? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var wine = await _context.Wines.FindAsync(id);
+            if (wine == null)
+            {
+                return NotFound();
+            }
+            return View(wine);
+        }
+
+        // POST: Vineyard/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(double id, [Bind("Id,Country,Description,Designation,Points,Price,Province,Region1,Region2,TasterName,TasterTwitterHandle,Title,Variety,Winery")] Wine wine)
+        {
+            if (id != wine.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(wine);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!WineExists(wine.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(wine);
+        }
+
+        // GET: Vineyard/Delete/5
+        public async Task<IActionResult> Delete(double? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var wine = await _context.Wines
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (wine == null)
+            {
+                return NotFound();
+            }
+
+            return View(wine);
+        }
+
+        // POST: Vineyard/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(double id)
+        {
+            var wine = await _context.Wines.FindAsync(id);
+            if (wine != null)
+            {
+                _context.Wines.Remove(wine);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool WineExists(double id)
+        {
+            return _context.Wines.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ In_Class_Examples/Module2_Exam_Review/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Module2_Exam_Review.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
